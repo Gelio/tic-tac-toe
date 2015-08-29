6d8d98a474d3ebcc53a83c733fa23d0fa1b5088b
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2015-08-29 09:57:34 +0200

    merged winning-positions and tile together, updated TODO

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 [A task](http://www.freecodecamp.com/challenges/zipline-build-a-tic-tac-toe-game) from FreeCodeCamp.
 
 ## TODO: ##
-* merge tile.js and winning-positions.js together for faster load time
 * AI
--- assets/js/tile.js
+++ assets/js/tile.js
@@ -11,3 +11,15 @@ function Tile(row, column) {
         this.number = row*3 + column;
     }
 }
+
+// Winning positions
+var winningPositionsArray = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
