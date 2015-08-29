3563f1d99903a24e526453efbd0f90f99c9fadf7
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2015-08-29 08:53:01 +0200

    added simple AI and click function, undesired effects, this commit is base for future improvements in other branches

--- main.js
+++ main.js
@@ -1,45 +1,116 @@
 (function() {
+    // Processing winning positions from just numbers to row/column format
+    var winningPositions = [];
+    winningPositionsArray.forEach(function(winningSet) {
+        var output = [];
+        winningSet.forEach(function(position) {
+            output.push([
+                Math.floor(position/3),
+                position%3
+            ]);
+        });
+        winningPositions.push(output);
+    });
+
+
     var app = angular.module("ticTacToe", ["ngAnimate"]);
 
-    app.controller("GameCtrl", ["$scope", function($scope) {
+    app.controller("GameCtrl", ["$scope", "$timeout", function($scope, $timeout) {
         $scope.playerSign = 1;  // 1 for X, -1 for O
-        $scope.board = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ];
-        $scope.highlighted = [
-            [false, false, false],
-            [false, false, false],
-            [false, false, false]
-        ];
+        var computerSign = -1;
+        $scope.board = [];
+        $scope.highlighted = [];
+
+        function newGame() {
+            $scope.highlighted = [
+                [false, false, false],
+                [false, false, false],
+                [false, false, false]
+            ];
+
+            $scope.board = [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
+            ];
+
+            if($scope.playerSign == -1) {
+                computerMove();
+            }
+        }
+
+        function computerMove() {
+            var available = [];
+            $scope.board.forEach(function(row, rowIndex) {
+                row.forEach(function(tile, tileIndex) {
+                    if(tile === 0) {
+                        available.push([rowIndex, tileIndex]);
+                    }
+                });
+            });
+
+            var randomed = Math.floor(Math.random()*available.length);
+            $scope.board[available[randomed][0]][available[randomed][1]] = computerSign;
+
+            checkWinningCondition();
+        }
+
+        function highlightTiles(tileSet, highlight) {
+            if(highlight === undefined)
+                highlight = true;
+
+            tileSet.forEach(function(tile) {
+                $scope.highlighted[tile[0]][tile[1]] = highlight;
+            });
+        }
+
+        function checkWinningCondition() {
+            winningPositions.forEach(function(winningSet) {
+                var sum = 0;
+                //console.log(winningSet);
+                winningSet.forEach(function(tile) {
+                    //console.log(tile);
+                    sum += $scope.board[tile[0]][tile[1]];
+                });
+
+                if(sum == -3) {
+                    // O wins
+                    highlightTiles(winningSet);
+                    $timeout(newGame, 2000);
+                }
+                else if(sum == 3) {
+                    // X wins
+                    highlightTiles(winningSet);
+                    $timeout(newGame, 2000);
+                }
+            });
+        }
+
+
+        newGame();
 
         $scope.changeSign = function(newSign) {
             $scope.playerSign = newSign;
+            if(newSign == 1)
+                computerSign = -1;
+            else
+                computerSign = 1;
+
+            newGame();
         };
 
         $scope.playTile = function(row, column) {
-            console.log("playing", row, column);
-
-            switch($scope.board[row][column]) {
-                case -1:
-                    $scope.board[row][column] = 0;
-                    break;
-
-                case 0:
-                    $scope.board[row][column] = 1;
-                    break;
-
-                case 1:
-                    $scope.board[row][column] = -1;
-                    break;
-
-                default:
-                    console.log("error, unknown value in board at row " + row + " and column " + column, $scope.board);
-                    break;
+            if($scope.board[row][column] !== 0) {
+                $scope.highlighted[row][column] = true;
+                $timeout(function() {
+                    $scope.highlighted[row][column] = false;
+                }, 300);
+                return;
             }
 
-            $scope.highlighted[row][column] = !$scope.highlighted[row][column];
+            $scope.board[row][column] = $scope.playerSign;
+            checkWinningCondition();
+            computerMove();
         };
     }]);
 })();
